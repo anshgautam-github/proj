0a4f1e0029d0030d4546a821814ef0bb3b16176e
Ansh Gautam <anshgautam1011@gmail.com>
2025-03-29 01:01:01 +0530

    partial header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import { SignedIn,SignInButton,SignedOut,SignUpButton,UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { LayoutDashboard } from 'lucide-react'
 
 const Header = () => {
+
   return (
-    <div>
+
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 
+    supports-[backdrop-filter]:bg-background/60'>
+
+        <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+            <Link href="/">
+                <Image
+                    src="/logo.png"
+                    width={200}
+                    height={60}
+                    className="h-12 py-1 w-auto object-contain"
+                />
+            </Link>
+        
+            <div>
+                <SignedIn>
+                    <Link href={"/dashboard"}>
+                        <Button>
+                            <LayoutDashboard className="h-4 w-4"/>
+                            <span className='hidden md:block'>Industry Insights</span>
+                        </Button>
+                    </Link>
+                </SignedIn>
+            </div>
+        </nav>
+
+
+
+
         <SignedOut>
             <SignInButton />    
         </SignedOut>
         <SignedIn>
                 <UserButton />
         </SignedIn>
-    </div>
+    </header>
     
   )
 }
