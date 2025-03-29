41e8fc7dadba951f004e308e5644171d4b13f86b
Ansh Gautam <anshgautam1011@gmail.com>
2025-03-29 14:52:21 +0530

    middleware

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { SignedIn,SignInButton,SignedOut,SignUpButton,UserButton } from '@clerk/nextjs'
+import { SignedIn,SignInButton,SignedOut,UserButton } from '@clerk/nextjs'
+import { DropdownMenu,DropdownMenuTrigger,DropdownMenuContent,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuItem} from './ui/dropdown-menu'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from './ui/button'
-import { LayoutDashboard } from 'lucide-react'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
 
 const Header = () => {
 
@@ -16,34 +17,76 @@ const Header = () => {
             <Link href="/">
                 <Image
                     src="/logo.png"
+                    alt='image'
                     width={200}
                     height={60}
                     className="h-12 py-1 w-auto object-contain"
                 />
             </Link>
         
-            <div>
+            <div className='flex items-center space-x-2 md:space-x-4'>
                 <SignedIn>
                     <Link href={"/dashboard"}>
-                        <Button>
+                        <Button variant="outline">
                             <LayoutDashboard className="h-4 w-4"/>
                             <span className='hidden md:block'>Industry Insights</span>
                         </Button>
                     </Link>
+                
+
+                <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                             <Button>
+                                <StarsIcon className="h-4 w-4"/>
+                                <span className='hidden md:block'>Growth Tools</span>
+                                <ChevronDown className="h-4 w-4"/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                <Link href={"/resume"} className='flex items-center gap-2'>
+                                    <FileText className="h-4 w-4"/>
+                                    <span>Build Resume</span>
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
+                                    <PenBox className="h-4 w-4"/>
+                                    <span>Cover Letter</span>
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href={"/interview"} className='flex items-center gap-2'>
+                                    <GraduationCap className="h-4 w-4"/>
+                                    <span>Interview Prep</span>
+                                </Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                </DropdownMenu>
+    
+                        <UserButton 
+                            appearance={{
+                                elements:{
+                                    avatarBox: "w-10 h-10",
+                                    userButtonPopoverCard: "shadow-xl",
+                                    userPreviewMainIdentifier:"font-semibold",
+                                }
+                            }}
+                            afterSignOutUrl='/'
+                        />
+             
                 </SignedIn>
+                    
+
+                <SignedOut>
+                    <SignInButton>
+                        <Button variant='outline'>Sign in</Button>    
+                    </SignInButton>    
+                </SignedOut>
+                
             </div>
         </nav>
-
-
-
-
-        <SignedOut>
-            <SignInButton />    
-        </SignedOut>
-        <SignedIn>
-                <UserButton />
-        </SignedIn>
-    </header>
+ </header>
     
   )
 }
